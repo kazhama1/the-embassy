9088008d3b12c45eebf439fcd404110c7bfc80ba
kazhama1 <kazhamamupuchi176@gmail.com>
2026-07-17 12:25:21 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,10 @@
+length=12
+width=8
+area= length * width
+perimiter= 2 + (length+width)
+print(f"area is: ", area)
+print(f"perimeter is: ", perimiter)
+
+#(Example on python for while loop)
+name=input("what is your name: ")
+print(int(name).upper)
